e0f94d08a92d2cd72e0bb3404a7e276416ac2f40
jexchan <jexchan@gmail.com>
2013-03-06 16:16:58 +0800

    set bundle configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,11 @@
+set :bundle_cmd, "/home/deployment/.rvm/gems/ruby-1.9.3-p286@global/bin/bundle"
+set :bundle_dir, "/home/deployment/.rvm/gems/ruby-1.9.3-p286"
+# set :bundle_dir,     ""         # install into "system" gems
+set :bundle_flags,   "--quiet"  # no verbose output
+set :bundle_without, []         # bundle all gems (even dev & test)
 require 'bundler/capistrano'
 require "rvm/capistrano"
 
-set :bundle_dir,     ""         # install into "system" gems
-set :bundle_flags,   "--quiet"  # no verbose output
-set :bundle_without, []         # bundle all gems (even dev & test)
 
 # set :rvm_ruby_string, 'ruby-1.9.3-p286@global'
 set :rvm_type, :system
